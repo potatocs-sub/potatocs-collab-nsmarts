d4ac9eba2431c3f58a0a09ae252f3a3edbb80c26
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-05-30 09:23:31 +0900

    [feat] page 새로고침 할 경우 loading 페이지 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SplashScreenService } from './services/splash-screen.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'nsmarts';
+
+  constructor(private splashScreenService: SplashScreenService) {}
 }
