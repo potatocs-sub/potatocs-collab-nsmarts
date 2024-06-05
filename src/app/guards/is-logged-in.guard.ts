a2623960b8cc75eec48f578208498973c200089c
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-06-05 11:59:03 +0900

    [feat] 가드 수정, find-pw css 수정

--- src/app/guards/is-logged-in.guard.ts
+++ src/app/guards/is-logged-in.guard.ts
@@ -1,40 +1,32 @@
 import { inject } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivateFn,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
-import { DialogService } from '../stores/dialog/dialog.service';
 
-export const isLoggedInGuard: CanActivateFn = (
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
-) => {
+export const isLoggedInGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  const dialogService = inject(DialogService);
   const router = inject(Router);
-  const routePath = route.routeConfig?.path ?? ''; // ?? 은 타입스크립트 문법으로 undefined || null 이면 ''로 주겠다.
-  const isLoggedIn = authService.isAuthenticated();
+  const routePath = state.url; // state.url을 사용하여 현재 경로를 가져옵니다.
 
-  if (isLoggedIn) {
-    if (['sign-in', 'find-pw', 'sign-up'].includes(routePath)) {
-      router.navigate(['companies']);
+  if (!authService.isAuthenticated()) {
+    if (
+      routePath === '/sign-in' ||
+      routePath === '/sign-up' ||
+      routePath === '/find-pw'
+    ) {
+      return true; // 인증되지 않은 사용자가 해당 경로에 접근하는 것은 허용
     }
-    return true;
+    router.navigate(['/sign-in']); // 인증되지 않은 사용자가 다른 경로에 접근하는 경우 로그인 페이지로 리디렉션
+    return false;
+  } else {
+    if (
+      routePath === '/sign-in' ||
+      routePath === '/sign-up' ||
+      routePath === '/find-pw' ||
+      routePath === '/'
+    ) {
+      router.navigate(['/companies']); // 이미 인증된 사용자가 해당 경로에 접근하려고 할 때 홈 페이지로 리디렉션
+      return false;
+    }
+    return true; // 인증된 사용자는 모든 경로에 접근 가능
   }
-
-  if (['sign-in', 'sign-up', 'find-pw'].includes(routePath)) {
-    return true;
-  }
-
-  if (routePath === '') {
-    router.navigate(['sign-in']);
-    return true;
-  }
-
-  dialogService.openDialogNegative('Please login first');
-  router.navigate(['sign-in'], { queryParams: { redirectURL: state.url } });
-  return false;
 };
